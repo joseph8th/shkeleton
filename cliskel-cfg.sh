36cb9759ecf8a24b4e5c97e08a6c7bc8daca2bed
Joseph Edwards VIII <joseph8th@notroot.us>
2014-09-27 12:57:31 -0400

    parse all opts bugfix + support for escape-quoted ARGS

--- cliskel-cfg.sh
+++ cliskel-cfg.sh
@@ -3,8 +3,8 @@
 ####  CONFIGURABLES  ######################################################
 
 # (0) SELF - The 'self' command. Has special status.
-#     a) like `cp -r SRC DEST` - the script IS the command
-#     b) `git -b checkout BRANCH` - the script RUNS command
+#     (a) like `cp -r SRC DEST` - the script IS the command
+#     (b) `git -b checkout BRANCH` - the script RUNS command
 
 SELF_HELP="Skeleton CLI wireframe."    # script summary long as you like
 
@@ -16,7 +16,6 @@ SELF_OPTS_HELP[h]="print help message"
 # SELF_ARGS
 
 SELF_ARGS=    # string: if COMMANDS defined, then leave empty
-
 
 # COMMANDS - first argument commands can have arguments and options
 # If not defining commands then leave blank: `COMMANDS=`
@@ -34,17 +33,18 @@ CMD_HELP[fling]="fling love from BAR at BAZ"
 CMD_ARGS[fling]="BAR BAZ"    # space-separated string of positional args
 CMD_OPTS[fling]="p poo t toss"    # string of pairwise short-long opts
 
-# (2.1) if CMD_OPTS[CMD] defined, optionally define optargs using short opt keys
+# (2.1) if CMD_OPTS[CMD] defined, optionally define optargs w opt keys
+
 CMD_OPTARGS[p]="POO"         # option args space-separated string
 CMD_OPTS_HELP[p]="BAR instead flings POO at BAZ"
 CMD_OPTS_HELP[t]="BAR instead tosses at BAZ"
 
+
 #--------------------------------------------------------------------------
 # COMMAND FUNCTIONS - must be named `_run_CMD` for each CMD in COMMANDS
 # Define each CMD function below. All your logic goes here.
-# Use globals $CMD $ARGS and $OPTS in your logic.
-#
-# i.e., `function _run_skel { ... }`
+# Use parsed CMD, OPTS[] with OPTARGS[opt], and ARGS[] here.
+
 
 function _run_self {
 
@@ -63,17 +63,19 @@ function _run_fling {
     verb="flings"
     noun="love"
 
-    for opt in "${OPTS}"; do
+    # loop over OPTS + OPTARGS parsed, but *validate* here in `run_CMD`
+    for opt in "${OPTS[@]}"; do
         case $opt in
             t)
                 verb="tosses"
                 ;;
             p)
                 # remove leading and trailing whitespace
-                noun=`echo "${OPTARGS[p]}" | sed -e 's/^[ \t]*//'`
+                noun=`echo "${OPTARGS[$opt]}" | sed -e 's/^[ \t]*//'`
                 ;;
         esac
     done
 
+    # this is what 'fling' cmd does...
     printf "\n_run_fling:\n\t${ARGS[BAR]} ${verb} ${noun} at ${ARGS[BAZ]}\n"
 }
