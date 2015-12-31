0bd9614698b60e27193ad14d05c35667fa292470
Joseph Edwards VIII <jedwards8th@gmail.com>
2015-12-31 16:27:47 -0500

    fixed regression caused by allowing pre-/post-option arguments

--- shkeleton.sh
+++ shkeleton.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-####  CONFIGURABLES  ######################################################
+####  CONFIGURABLES  #################################################
 
 DEBUG=1
 MAN_HELP=1    # 0=text help, 1=man help
@@ -11,32 +11,45 @@ VERSION="0.2b"
 AUTHOR="Joseph Edwards VIII"
 AUTHOR_EMAIL="jedwards8th@gmail.com"
 
-# (0) SELF - The 'self' command. Has special status.
-#     (a) like `cp -r SRC DEST` - the script IS the command
-#     (b) like `git -b checkout BRANCH` - the script RUNS command
+
+####  SELF  ##########################################################
+
+# The 'self' command. Has special status.
+#   (a) like `cp -r SRC DEST` - the script IS the command
+#   (b) like `git -b checkout BRANCH` - the script RUNS command
 
 # using man so prefix '-' with an escape '\'
 SELF_NAME="${SCRIPT} \- CLI shell scripting for lazy people"
 
 # script summary long as you like
-SELF_HELP="A skeletal wireframe to speed development of command-line interface (CLI) executable bash scripts. Intended to speed coding for more than the simplest command-line scripts. Use whenever 'optargs' isn't enough, or when the number of parameters is too unwieldy for positional arguments, or when one doesn't feel like doing CLI argument parsing, at all."
+SELF_HELP="A skeletal wireframe to speed development of command-line
+interface (CLI) executable bash scripts. Intended to speed coding for
+more than the simplest command-line scripts. Use whenever 'optargs'
+isn't enough, or when you want to use GNU style long arguments, or
+when the number of parameters is too unwieldy for positional
+arguments, or when the script(s) you're writing are already complex,
+or when you don't just feel like doing CL argument parsing."
 
-# SELF_ARGS
+# SELF_ARGS - if COMMANDS defined, then define empty: `SELF_ARGS=`
+SELF_ARGS="POOP"
 
-SELF_ARGS="POOP"    # string: if COMMANDS defined, then leave empty
+# SELF_OPTS_NOARG - singletons: ARGS ignored, and only 1st OPT parsed
+SELF_OPTS_NOARG="h help v version"
+SELF_OPTS_NOARG_HELP[h]="print help message"
+SELF_OPTS_NOARG_HELP[v]="print version information"
 
-# SELF_OPTS
-
-SELF_OPTS="h help t target"       # pairwise short-long options ie, (-h, --help)
-SELF_OPTS_HELP[h]="print help message"
+# SELF_OPTS - pairs short-long OPTS (ie, "t target" == -t, --target)
+SELF_OPTS="t target"
 SELF_OPTS_HELP[t]="target project path"
 
-# SELF_OPTARGS
-
+# SELF_OPTARGS - string of positional option arguments
 SELF_OPTARGS[t]="SRCDIR TARGETDIR"
 
-# COMMANDS - first argument commands can have arguments and options
-# If not defining commands then leave blank: `COMMANDS=`
+
+####  COMMANDS  ######################################################
+
+# Command mode: first parameter commands can have ARGS and OPTS.
+# If not defining commands then define empty: `COMMANDS=`
 
 #COMMANDS=( "skel" "fling" )    # array of strings
 COMMANDS=
@@ -58,17 +71,32 @@ COMMANDS=
 #CMD_OPTS_HELP[p]="SRC instead flings POO at DEST"
 #CMD_OPTS_HELP[t]="SRC instead tosses at DEST"
 
-# Additional manpage sections (optional)
-FILES="The 'skel' command copies files in './skel' directory (or the current scripts) to the given destination path, renaming them in the process."
+
+####  OPTIONAL MANPAGE  ##############################################
+
+FILES="The 'skel' command copies files in './skel' directory (or the
+current scripts) to the given destination path, renaming them in the
+process."
+
 ENVIRONMENT=
-EXIT_STATUS="Exits with status \$NOERR (0 for OK, and >0 with errors)."
+
+EXIT_STATUS="Exits with status \$ERRNO."
+
 EXAMPLE_01="${SCRIPT} skel /path/to/new/script"
-EXAMPLE_02="${SCRIPT} fling -t -p poo dirt \(rs\"Phil Collins\(rs\" \(rs\"Batman and Robin\(rs\""
+
+EXAMPLE_02="${SCRIPT} fling -t -p poo dirt
+\(rs\"Phil Collins\(rs\" \(rs\"Batman and Robin\(rs\""
+
 EXAMPLES="\&${EXAMPLE_01}\n\n\&${EXAMPLE_02}"
-BUGS="Quotes must be escaped for multiple word arguments. Only double quotes are supported."
+
+BUGS="Quotes must be escaped for multiple word arguments. Only double
+quotes are supported."
+
 SEE_ALSO=
 
-####  COMMAND FUNCTIONS  ##################################################
+
+####  COMMAND FUNCTIONS  #############################################
+
 # Must be named `_run_CMD` for each CMD in COMMANDS.
 # Define each CMD function below. All your logic goes here.
 # Use parsed CMD, OPTS[] with OPTARGS[opt], and ARGS[] here.
@@ -79,12 +107,14 @@ function _run_self {
         case $opt in
             h)
                 _print_help
+                _exit_err
                 ;;
             t)
                 echo ${OPTARGS[t]}
                 ;;
             *)
                 _print_help
+                _exit_err
                 ;;
         esac
     done
