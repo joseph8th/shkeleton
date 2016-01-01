4b9aa711ae107d31ff267b11260ca39ee4027abc
Joseph Edwards VIII <jedwards8th@gmail.com>
2016-01-01 15:30:56 -0500

    finally fixed almost all bugs AFAIK - except escape-quoted multi-word params

--- shkeleton.sh
+++ shkeleton.sh
@@ -5,7 +5,7 @@
 DEBUG=1
 MAN_HELP=1    # 0=text help, 1=man help
 MAN_TYPE=1    # a command by man standards
-VERSION="0.2b"
+VERSION="1.0"
 
 # Author metadata
 AUTHOR="Joseph Edwards VIII"
@@ -31,19 +31,20 @@ arguments, or when the script(s) you're writing are already complex,
 or when you don't just feel like doing CL argument parsing."
 
 # SELF_ARGS - if COMMANDS defined, then define empty: `SELF_ARGS=`
-SELF_ARGS="POOP"
+#SELF_ARGS="DEST POOP CRAP"
+SELF_ARGS=
 
 # SELF_OPTS_NOARG - singletons: ARGS ignored, and only 1st OPT parsed
 SELF_OPTS_NOARG="h help v version"
-SELF_OPTS_NOARG_HELP[h]="print help message"
-SELF_OPTS_NOARG_HELP[v]="print version information"
+SELF_OPTS_HELP[h]="print help message"
+SELF_OPTS_HELP[v]="print version information"
 
 # SELF_OPTS - pairs short-long OPTS (ie, "t target" == -t, --target)
-SELF_OPTS="t target"
-SELF_OPTS_HELP[t]="target project path"
+#SELF_OPTS="f foo"
+#SELF_OPTS_HELP[f]="example self-as-command option"
 
 # SELF_OPTARGS - string of positional option arguments
-SELF_OPTARGS[t]="SRCDIR TARGETDIR"
+#SELF_OPTARGS[f]="BAR BAZ"
 
 
 ####  COMMANDS  ######################################################
@@ -51,25 +52,25 @@ SELF_OPTARGS[t]="SRCDIR TARGETDIR"
 # Command mode: first parameter commands can have ARGS and OPTS.
 # If not defining commands then define empty: `COMMANDS=`
 
-#COMMANDS=( "skel" "fling" )    # array of strings
-COMMANDS=
+COMMANDS=( "skel" "fling" )    # array of strings
+#COMMANDS=
 
 # (1) 'skel' command with one arg, no opts
 
-#CMD_HELP[skel]="copy './skel' to DEST, where DEST is a new directory"
-#CMD_ARGS[skel]="DEST"   # define both CMD_OPTS[CMD] and CMD_ARGS[CMD] for
+CMD_HELP[skel]="create skeleton project in directory DEST"
+CMD_ARGS[skel]="DEST"
 
 # (2) 'fling' command with two args, one opt -- a dummy command
 
-#CMD_HELP[fling]="fling love from SRC at DEST"
-#CMD_ARGS[fling]="SRC DEST"    # space-separated string of positional args
-#CMD_OPTS[fling]="p poo t toss"    # string of pairwise short-long opts
+CMD_HELP[fling]="fling love from SRC at DEST"
+CMD_ARGS[fling]="SRC DEST"    # space-separated string of positional args
+CMD_OPTS[fling]="p poo t toss"    # string of pairwise short-long opts
 
 # (2.1) if CMD_OPTS[CMD] defined, optionally define optargs w opt keys
 
-#CMD_OPTARGS[p]="POO BAH"         # option args space-separated string
-#CMD_OPTS_HELP[p]="SRC instead flings POO at DEST"
-#CMD_OPTS_HELP[t]="SRC instead tosses at DEST"
+CMD_OPTARGS[p]="POO BAH"         # option args space-separated string
+CMD_OPTS_HELP[p]="SRC instead flings POO at DEST"
+CMD_OPTS_HELP[t]="SRC instead tosses at DEST"
 
 
 ####  OPTIONAL MANPAGE  ##############################################
@@ -103,35 +104,57 @@ SEE_ALSO=
 
 # _run_self should always be defined and should minimally _print_help
 function _run_self {
+    _debug "\n_run_self:\n"
+    _str_equal "$CMD" "self" && self_as_command && _exit_err
+}
+
+# example of self-as-command
+function self_as_command {
+
+    if ! _is_empty "${ARGS[@]}"; then
+        cmd_args=( ${CMD_ARGS[@]} )
+
+        # NOTICE the use of 'eval' to get ARGS values
+        for arg in "${cmd_args[@]}"; do
+            eval val=${ARGS[$arg]}
+            echo "${arg}: ${val}"
+        done
+    fi
+
     for opt in "${OPTS}"; do
         case $opt in
             h)
                 _print_help
                 _exit_err
                 ;;
-            t)
-                echo ${OPTARGS[t]}
+            v)
+                echo "$SCRIPT $VERSION"
+                _exit_err
+                ;;
+            f)
+                # NOTICE the use of 'eval' to get OPTARGS array
+                eval fopts=( ${OPTARGS[f]} )
+                echo "BAR: ${fopts[0]}"
+                echo "BAZ: ${fopts[1]}"
                 ;;
             *)
                 _print_help
-                _exit_err
+                _err; _exit_err
                 ;;
         esac
     done
-
-    echo ${ARGS[@]}
 }
 
 # copy the skel directory and files to chosen destination
-function _run_skel {
-
-    # default try to copy out of ./skel directory (unconfigured)
-    src='./skel'
-    dest="${ARGS[DEST]}"
+function copy_skel {
+    dest="$1"
     skelname=`basename ${dest}`
 
+    # default try to copy files out of ./skel directory
+    src="$(readlink -m ./skel)"
+
     # if no 'skel' dir then use current directory (these files)
-    [[ ! -d "${src}" ]] && src='./'
+    [[ ! -d "${src}" ]] && src="$(readlink -m ./)"
 
     if [[ ! -f "${src}/${SCRIPT}" || ! -f "${src}/${SCRIPT}.sh" ]]; then
         _err; _exit_err "'${SCRIPT}' skel files not found."
@@ -139,9 +162,29 @@ function _run_skel {
 
     # now copy the files over
     echo "Copying ${src} directory to new directory ${dest} ..."
-    [[ ! -d "${dest}" ]] && mkdir "${dest}"
+
+    [[ ! -d "${dest}" ]] && mkdir -p "${dest}"
+    if [[ ! -d "${dest}" ]]; then
+        _err; _exit_err "unable to create directory '${dest}'"
+    fi
+
     cp "${src}/${SCRIPT}" "${dest}/${skelname}"
+    if [[ ! -f "${dest}/${skelname}" ]]; then
+        _err; exit_err "unable to copy '${SCRIPT}' to '${dest}/${skelname}'"
+    fi
+
     cp "${src}/${SCRIPT}.sh" "${dest}/${skelname}.sh"
+    if [[ ! -f "${dest}/${skelname}.sh" ]]; then
+        _err; exit_err "unable to copy '${SCRIPT}.sh' to '${dest}/${skelname}.sh'"
+    fi
+
+    echo "SUCCESS!"
+}
+
+function _run_skel {
+    eval dest=${ARGS[DEST]}
+    copy_skel "$dest"
+
 }
 
 # example command function using options and option arguments
@@ -158,7 +201,8 @@ function _run_fling {
                 verb="tosses"
                 ;;
             p)
-                optargs=( ${OPTARGS[$opt]} )
+                # NOTICE use of 'eval' to get OPTARGS array
+                eval optargs=( ${OPTARGS[$opt]} )
                 noun1="${optargs[0]}"
                 noun2="${optargs[1]}"
                 ;;
@@ -167,5 +211,9 @@ function _run_fling {
 
     # this is what 'fling' cmd does...
     _debug "\n_run_fling:\n"
-    printf "\n${ARGS[SRC]} ${verb} ${noun1} and ${noun2} at ${ARGS[DEST]}.\n"
+
+    # NOTICE use of 'eval' to get ARGS values
+    eval src=${ARGS[SRC]}
+    eval dest=${ARGS[DEST]}
+    printf "${src} ${verb} ${noun1} and ${noun2} at ${dest}.\n"
 }
